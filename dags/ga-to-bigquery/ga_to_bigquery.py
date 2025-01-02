3fa7bbf8980c6eb33b70633a194af94ea0a579bc
Sagar Sarkale <sagarsarkale.work@gmail.com>
2025-01-03 03:32:51 +0530

    [add] individual folders for custom dags

---
+++ dags/ga-to-bigquery/ga_to_bigquery.py
@@ -0,0 +1,37 @@
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime
+
+
+def print_hello():
+    print("Hello, World!")
+
+
+def print_date():
+    print(datetime.now())
+
+
+default_args = {
+    "owner": "airflow",
+    "start_date": datetime(2023, 10, 1),
+}
+
+dag = DAG(
+    "hello_world_dag",
+    default_args=default_args,
+    schedule_interval="@daily",
+)
+
+hello_task = PythonOperator(
+    task_id="print_hello",
+    python_callable=print_hello,
+    dag=dag,
+)
+
+date_task = PythonOperator(
+    task_id="print_date",
+    python_callable=print_date,
+    dag=dag,
+)
+
+hello_task >> date_task
